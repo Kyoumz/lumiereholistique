91eebff73ccccec3c44592257fd7b7ded2ddd685
adam.sebbat <adam.sebbat@uha.fr>
2025-03-25 16:47:48 +0100

    aa

--- frontend/src/app/pages/accueil/accueil.component.ts
+++ frontend/src/app/pages/accueil/accueil.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '../../components/header/header.component';  // Vérifie le chemin
-import { FooterComponent } from '../../components/footer/footer.component';  // Vérifie le chemin
+import { HeaderComponent } from '../../components/header/header.component';  
+import { FooterComponent } from '../../components/footer/footer.component'; 
 
 @Component({
   selector: 'app-accueil',
