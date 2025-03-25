374d27e283c0bd635d43958dff347e5f0f56f883
adam.sebbat <adam.sebbat@uha.fr>
2025-03-25 11:20:29 +0100

    nodedaemon + acceuil front

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
+    res.json({ message: 'aa' });
 });
 
 const PORT = process.env.PORT || 5000;
