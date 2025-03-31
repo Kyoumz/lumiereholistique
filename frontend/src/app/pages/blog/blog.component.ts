478c25a0d82aef272833e547aae536ad7e0ab0e0
adam.sebbat <adam.sebbat@uha.fr>
2025-03-31 17:02:15 +0200

    fin journée

--- frontend/src/app/pages/blog/blog.component.ts
+++ frontend/src/app/pages/blog/blog.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '../../components/header/header.component';  
-import { FooterComponent } from '../../components/footer/footer.component'; 
-
-
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { BlogService } from '../../services/blog.service';
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent, CommonModule], 
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent {
+  articles: any[] = [];
 
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit() {
+    console.log('ngOnInit() appelé');
+    this.blogService.getArticles().subscribe(
+      (data: any[]) => {
+        this.articles = data;
+        console.log('Articles récupérés :', data);
+      },
+      (error: any) => {
+        console.error('Erreur lors du chargement des articles', error);
+      }
+    );
+  }
 }
