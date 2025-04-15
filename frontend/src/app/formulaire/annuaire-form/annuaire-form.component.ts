bd7d199b388faeeb5f999331d50bc7f85c95809d
adam.sebbat <adam.sebbat@uha.fr>
2025-04-15 10:54:23 +0200

    video podcast + annuaire

---
+++ frontend/src/app/formulaire/annuaire-form/annuaire-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-annuaire-form',
+  imports: [],
+  templateUrl: './annuaire-form.component.html',
+  styleUrl: './annuaire-form.component.scss'
+})
+export class AnnuaireFormComponent {
+
+}
---
+++ frontend/src/app/formulaire/video-podcast-form/video-podcast-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-podcast-form',
+  imports: [],
+  templateUrl: './video-podcast-form.component.html',
+  styleUrl: './video-podcast-form.component.scss'
+})
+export class VideoPodcastFormComponent {
+
+}
